d419c4a179372150411511269e55ce25a1d3f6cf
Brooks Vaughn <18422308+BrooksV@users.noreply.github.com>
2025-02-01 16:52:54 -0500

    Improved Copy-ProjectResource function to copy only folder contents

--- src/private/CopyProjectResource.ps1
+++ src/private/CopyProjectResource.ps1
@@ -3,9 +3,12 @@ function Copy-ProjectResource {
     $resFolder = [System.IO.Path]::Join($data.ProjectRoot, 'src', 'resources')
 
     if (Test-Path $resFolder) {
-        if (Get-ChildItem $resFolder -ErrorAction SilentlyContinue) {
-            Write-Verbose 'Files found in resource folder, Copying resource folder content'
-            Copy-Item -Path $resFolder -Destination ($data.OutputModuleDir) -Recurse -Force -ErrorAction Stop
+        $items = Get-ChildItem -Path $resFolder -ErrorAction SilentlyContinue
+        if ($items) {
+            Write-Verbose 'Files found in resource folder, copying resource folder content'
+            foreach ($item in $items) {
+                Copy-Item -Path $item.FullName -Destination ($data.OutputModuleDir) -Recurse -Force -ErrorAction Stop
+            }
         }
     }
 }
