fbb1e4edf8af5e1983a13e78131657433d64756a
Manjunath Beli <belibug@outlook.com>
2025-02-25 14:16:44 -0600

    Revert "Merge pull request #10 from BrooksV/update-copy-projectresource"
    This reverts commit 0b19ead557f0770096b05d7eba6c36a87e7b07e8, reversing
    changes made to ebcd9783d949cfc356fc065cd5aa6bed477f1263.
    
    Tracking these changes as new branch

--- src/private/CopyProjectResource.ps1
+++ src/private/CopyProjectResource.ps1
@@ -1,22 +1,11 @@
 function Copy-ProjectResource {
     $data = Get-MTProjectInfo
     $resFolder = [System.IO.Path]::Join($data.ProjectRoot, 'src', 'resources')
+
     if (Test-Path $resFolder) {
-        if ($data.ResourceCopyMode -eq 'Content') {
-            # Copy the resources folder content to the OutputModuleDir
-            $items = Get-ChildItem -Path $resFolder -ErrorAction SilentlyContinue
-            if ($items) {
-                Write-Verbose 'Files found in resource folder, copying resource folder content'
-                foreach ($item in $items) {
-                    Copy-Item -Path $item.FullName -Destination ($data.OutputModuleDir) -Recurse -Force -ErrorAction Stop
-                }
-            }
-        } else {
-            # Copy the resources folder to the OutputModuleDir
-            if (Get-ChildItem $resFolder -ErrorAction SilentlyContinue) {
-                Write-Verbose 'Files found in resource folder, Copying resource folder'
-                Copy-Item -Path $resFolder -Destination ($data.OutputModuleDir) -Recurse -Force -ErrorAction Stop
-            }
+        if (Get-ChildItem $resFolder -ErrorAction SilentlyContinue) {
+            Write-Verbose 'Files found in resource folder, Copying resource folder content'
+            Copy-Item -Path $resFolder -Destination ($data.OutputModuleDir) -Recurse -Force -ErrorAction Stop
         }
     }
 }
