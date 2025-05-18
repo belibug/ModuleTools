fecf4bafedca2c7ab1d29bb267746f4a1c39048d
djs-zmtc [RAZORBACK] <github@zmtcreative.com>
2025-05-18 18:45:06 -0400

    add check for Class member functions or functions inside functions

--- src/private/GetFunctionNameFromFile.ps1
+++ src/private/GetFunctionNameFromFile.ps1
@@ -3,7 +3,13 @@ function Get-FunctionNameFromFile {
     try {
         $moduleContent = Get-Content -Path $filePath -Raw
         $ast = [System.Management.Automation.Language.Parser]::ParseInput($moduleContent, [ref]$null, [ref]$null)
-        $functionName = $ast.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $false) | ForEach-Object { $_.Name } 
+        $functionName = $ast.FindAll({
+            $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] -and
+            # Ignore Functions defined inside other Functions (e.g., nested functions or Class methods)
+            ($PSVersionTable.PSVersion.Major -lt 5 -or
+            $args[0].Parent -isnot [System.Management.Automation.Language.FunctionMemberAst])
+        }, $false) | ForEach-Object { $_.Name }
+
         return $functionName
     }
     catch { return '' }
